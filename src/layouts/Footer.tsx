f4d1612dd6b218adbcae83121a1978dee480002c
Lada496 <67321065+Lada496@users.noreply.github.com>
2023-01-18 20:24:23 -0800

    fixed footer style

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,7 +3,7 @@ import { Center, Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box as='footer' width='100%' bg='white'>
+    <Box as='footer' width='100%' bg='white' m={2}>
       <Center>&copy; Lada496</Center>
     </Box>
   )
