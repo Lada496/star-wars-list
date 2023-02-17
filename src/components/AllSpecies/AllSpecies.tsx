3f9b1220de572be94d56dbc7acba9c55ed99446c
Lada496 <67321065+Lada496@users.noreply.github.com>
2023-02-16 22:39:45 -0800

    just make filter work

--- src/components/AllSpecies/AllSpecies.tsx
+++ src/components/AllSpecies/AllSpecies.tsx
@@ -1,23 +1,62 @@
 import React from 'react'
-import { Heading, Select, Box } from '@chakra-ui/react'
+import { Heading, Select, Box, SimpleGrid } from '@chakra-ui/react'
 import { ModifiedCharacter } from '../../api/starWars-types'
-import useModifyCharacters, { SORT_FACTOR, SortFactor } from '../../hooks/useModifyCharacters'
+import useModifyCharacters, {
+  SORT_FACTOR,
+  FILTER_FACTER,
+  SortFactor,
+  FilterFactor,
+} from '../../hooks/useModifyCharacters'
 import CharacterList from '../CharacterList/CharacterList'
 
 type AllSpeciesProps = {
   characters: ModifiedCharacter[]
 }
 
+const filterTargets = (characters: ModifiedCharacter[], filterFactor: FilterFactor) => {
+  const filterTargetsObject = characters.reduce(
+    (acc: { [key: string]: string }, character: ModifiedCharacter) => {
+      const value = character[filterFactor]
+      if (!value || typeof value === 'undefined') {
+        return acc
+      } else if (!acc[value]) {
+        acc[value] = value
+      }
+      return acc
+    },
+    {},
+  )
+  return Object.keys(filterTargetsObject)
+}
+
 const AllSpecies = ({ characters }: AllSpeciesProps) => {
   const { state, dispatch } = useModifyCharacters({
     originalCharacters: characters,
-    modifiedCharacters: characters,
-    selectedValue: '',
+    renderedCharacters: characters,
+    sortFactor: '',
+    homeworldFilters: filterTargets(characters, FILTER_FACTER.HOMEWORLD),
+    genderFilters: filterTargets(characters, FILTER_FACTER.GENDER),
+    filterTargets: {
+      homelandTarget: '',
+      genderTarget: '',
+    },
   })
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  console.log(characters.filter((character) => character.gender !== 'male'))
+
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value as SortFactor
     dispatch({ type: 'sort', sortFactor: value })
+  }
+
+  const handleHomeworldFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    dispatch({ type: 'filter', filterFactor: FILTER_FACTER.HOMEWORLD, filterTarget: value })
+  }
+
+  const handleGengerFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value
+    dispatch({ type: 'filter', filterFactor: FILTER_FACTER.GENDER, filterTarget: value })
   }
 
   return (
@@ -25,17 +64,41 @@ const AllSpecies = ({ characters }: AllSpeciesProps) => {
       <Heading as='h1' size='lg'>
         All characters
       </Heading>
-      <Select placeholder='Sort by' value={state.selectedValue} onChange={handleChange}>
-        {Object.keys(SORT_FACTOR).map((sortFactorKey) => (
-          <option
-            key={sortFactorKey}
-            value={SORT_FACTOR[sortFactorKey as keyof typeof SORT_FACTOR]}
-          >
-            {SORT_FACTOR[sortFactorKey as keyof typeof SORT_FACTOR]}
-          </option>
-        ))}
-      </Select>
-      <CharacterList characters={state.modifiedCharacters} />
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing='10px'>
+        <Select placeholder='Sort by' value={state.sortFactor} onChange={handleSortChange}>
+          {Object.keys(SORT_FACTOR).map((sortFactorKey) => (
+            <option
+              key={sortFactorKey}
+              value={SORT_FACTOR[sortFactorKey as keyof typeof SORT_FACTOR]}
+            >
+              {SORT_FACTOR[sortFactorKey as keyof typeof SORT_FACTOR]}
+            </option>
+          ))}
+        </Select>
+        <Select
+          placeholder='Filter by characters homeland'
+          value={state.filterTargets.homelandTarget}
+          onChange={handleHomeworldFilter}
+        >
+          {state.homeworldFilters.map((filterElement) => (
+            <option key={filterElement} value={filterElement}>
+              {filterElement}
+            </option>
+          ))}
+        </Select>
+        <Select
+          placeholder='Filter by characters gender'
+          value={state.filterTargets.genderTarget}
+          onChange={handleGengerFilter}
+        >
+          {state.genderFilters.map((filterElement) => (
+            <option key={filterElement} value={filterElement}>
+              {filterElement}
+            </option>
+          ))}
+        </Select>
+      </SimpleGrid>
+      <CharacterList characters={state.renderedCharacters} />
     </Box>
   )
 }
--- src/hooks/useModifyCharacters.ts
+++ src/hooks/useModifyCharacters.ts
@@ -2,44 +2,154 @@ import { useReducer } from 'react'
 
 import { ModifiedCharacter } from '../api/starWars-types'
 
+export const CHARACTER_ACTION_TYPES = {
+  SORT: 'sort',
+  FILTER: 'filter',
+} as const
+
 export const SORT_FACTOR = {
   MASS: 'mass',
   HEIGHT: 'height',
 } as const
 
+export const FILTER_FACTER = {
+  HOMEWORLD: 'homeworld',
+  GENDER: 'gender',
+} as const
+
 // "mass" | "height"
 export type SortFactor = typeof SORT_FACTOR[keyof typeof SORT_FACTOR]
 
-type CharacterState = {
-  originalCharacters: ModifiedCharacter[]
-  modifiedCharacters: ModifiedCharacter[]
-  selectedValue: SortFactor | ''
+export type FilterFactor = typeof FILTER_FACTER[keyof typeof FILTER_FACTER]
+
+type FilterTargets = {
+  homelandTarget: string
+  genderTarget: string
 }
 
-type CharacterAction = { type: 'sort'; sortFactor: CharacterState['selectedValue'] }
+type CharacterState = {
+  originalCharacters: ModifiedCharacter[]
+  renderedCharacters: ModifiedCharacter[]
+  sortFactor: SortFactor | ''
+  homeworldFilters: string[]
+  genderFilters: string[]
+  filterTargets: FilterTargets
+}
+
+type CharacterAction =
+  | {
+      type: 'sort'
+      sortFactor: CharacterState['sortFactor']
+    }
+  | { type: 'filter'; filterFactor: FilterFactor; filterTarget: string }
 
 const sort = (characters: ModifiedCharacter[], key: SortFactor) =>
   characters.sort((a, b) => (b[key] || 0) - (a[key] || 0))
 
+const filter = (characters: ModifiedCharacter[], key: FilterFactor, filterTarget: string) =>
+  characters.filter((character) => character[key] === filterTarget)
+
+const hasTarget = (
+  filterFactor: FilterFactor,
+  previousHomelandTarget: string,
+  previousGenderTarget: string,
+) => {
+  let hasTarget = false
+  if (filterFactor === FILTER_FACTER.HOMEWORLD && previousHomelandTarget !== '') {
+    hasTarget = true
+  }
+  if (filterFactor === FILTER_FACTER.GENDER && previousGenderTarget !== '') {
+    hasTarget = true
+  }
+  return hasTarget
+}
+
 const characterReducer = (state: CharacterState, action: CharacterAction): CharacterState => {
   switch (action.type) {
-    case 'sort':
+    case CHARACTER_ACTION_TYPES.SORT: {
       if (action.sortFactor === 'height' || action.sortFactor === 'mass') {
-        const newModifiedCharacters = [...state.modifiedCharacters]
-        sort(newModifiedCharacters, action.sortFactor)
+        const newRenderedCharacters = [...state.renderedCharacters]
+        sort(newRenderedCharacters, action.sortFactor)
         return {
-          originalCharacters: state.originalCharacters,
-          modifiedCharacters: newModifiedCharacters,
-          selectedValue: action.sortFactor,
+          ...state,
+          renderedCharacters: newRenderedCharacters,
+          sortFactor: action.sortFactor,
         }
       }
-      return {
-        originalCharacters: state.originalCharacters,
-        modifiedCharacters: state.originalCharacters,
-        selectedValue: action.sortFactor,
+      const homelandTarget = state.filterTargets.homelandTarget
+      const genderTarget = state.filterTargets.genderTarget
+      let newRenderedCharacters = state.originalCharacters
+      if (homelandTarget) {
+        newRenderedCharacters = filter(
+          newRenderedCharacters,
+          FILTER_FACTER.HOMEWORLD,
+          homelandTarget,
+        )
       }
+      if (genderTarget) {
+        newRenderedCharacters = filter(newRenderedCharacters, FILTER_FACTER.GENDER, genderTarget)
+      }
+      return {
+        ...state,
+        sortFactor: action.sortFactor,
+        renderedCharacters: newRenderedCharacters,
+      }
+    }
 
     // TODO: add filter logic
+    case CHARACTER_ACTION_TYPES.FILTER: {
+      const filterFactor = action.filterFactor
+      const filterTarget = action.filterTarget
+      const sortFactor = state.sortFactor
+      const previousHomelandTarget = state.filterTargets.homelandTarget
+      const previousGenderTarget = state.filterTargets.genderTarget
+      if (filterTarget) {
+        let newRenderedCharacters: ModifiedCharacter[]
+
+        if (hasTarget(filterFactor, previousHomelandTarget, previousGenderTarget)) {
+          newRenderedCharacters = filter(state.originalCharacters, filterFactor, filterTarget)
+        } else {
+          newRenderedCharacters = filter(state.renderedCharacters, filterFactor, filterTarget)
+        }
+
+        if (sortFactor) {
+          sort(newRenderedCharacters, sortFactor)
+        }
+
+        return {
+          ...state,
+          renderedCharacters: newRenderedCharacters,
+          filterTargets: {
+            homelandTarget:
+              filterFactor === FILTER_FACTER.HOMEWORLD
+                ? filterTarget
+                : state.filterTargets.homelandTarget,
+            genderTarget:
+              filterFactor === FILTER_FACTER.GENDER
+                ? filterTarget
+                : state.filterTargets.genderTarget,
+          },
+        }
+      }
+      let newRenderedCharacters = state.originalCharacters
+      if (sortFactor) {
+        newRenderedCharacters = [...newRenderedCharacters]
+        sort(newRenderedCharacters, sortFactor)
+      }
+      return {
+        ...state,
+        renderedCharacters: newRenderedCharacters,
+        filterTargets: {
+          homelandTarget:
+            filterFactor === FILTER_FACTER.HOMEWORLD
+              ? filterTarget
+              : state.filterTargets.homelandTarget,
+          genderTarget:
+            filterFactor === FILTER_FACTER.GENDER ? filterTarget : state.filterTargets.genderTarget,
+        },
+      }
+    }
+
     default:
       return state
   }
